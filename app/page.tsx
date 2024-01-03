0461617043ae13e39cc861037df371139f11fc9e
RedBarz <barzcorp.fx@gmail.com>
2024-01-03 19:02:29 +0100

    desktop portfolio with typescript

---
+++ app/page.tsx
@@ -0,0 +1,77 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main className='w-screen h-screen relative'>
+      <div className='flex items-center w-full h-full bg-cover bg-center' style={{ backgroundImage: "url(main-bg.webp)" }}>
+        <div className='pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]'>
+          <h1 className='text-[50px] max-lg:text-[12px] text-white font-semibold'>
+            Make anything possible with <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 max-lg:text-[16px] max-lg:text-center'>
+              Web Development
+            </span>
+          </h1>
+          <p className='text-gray-200 hidden md:block'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et, vel? Possimus delectus quidem quaerat neque repudiandae commodi ex hic modi soluta sint. Velit distinctio eos autem earum sed itaque voluptates?
+          </p>
+          <div className='flex-col md:flex-row hidden md:flex gap-5'>
+            <Link href="/my-skills" className='rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]'>
+              Learn more
+            </Link>
+            <Link href="/my-projects" className='rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]'>
+              <div className='absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20' />
+              My Projects
+            </Link>
+            <Link href="/contact-me" className='rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]'>
+              <div className='absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20' />
+              Contact me
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className='absolute bottom-10 z-[20] right-5 flex flex-col md:hidden gap-5'>
+        <Link href="/my-skills" className='rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]'>
+          Learn more
+        </Link>
+        <Link href="/my-projects" className='rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]'>
+          My Projects
+        </Link>
+        <Link href="/contact-me" className='rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]'>
+          Contact me
+        </Link>
+      </div>
+      <div className='absolute bottom-0 right-0 z-[10]'>
+        <Image
+          src="/horse.png"
+          alt="horse"
+          height={300}
+          width={300}
+          className='absolute right-55 top-40'
+        />
+        <Image
+          src="/cliff.webp"
+          alt="cliff"
+          width={480}
+          height={480}
+        />
+      </div>
+      <div className='absolute bottom-0 z-[5] w-full h-full'>
+        <Image
+          src="/trees.webp"
+          alt='trees'
+          width={2000}
+          height={2000}
+          className='h-full w-full'
+        />
+      </div>
+      <Image
+        src="/stars.png"
+        alt="stars"
+        height={300}
+        width={300}
+        className='absolute top-10 left-0 z-[10]'
+      />
+
+    </main>
+  )
+};
